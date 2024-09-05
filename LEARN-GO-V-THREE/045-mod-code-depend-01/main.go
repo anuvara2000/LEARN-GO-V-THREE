df8582a09bb3ea3d90dc4d6d1869e3963426dc20
Bobbili <anuvara2000@gmail.com>
2024-09-05 15:28:25 +0530

    drop by drop, the bucket gets filled

--- LEARN-GO-V-THREE/045-mod-code-depend-01/main.go
+++ LEARN-GO-V-THREE/045-mod-code-depend-01/main.go
@@ -10,7 +10,13 @@ func main() {
 	  s2 :=puppy.Barks()
 	  fmt.Println(s1)
 	  fmt.Println(s2)
+       
+	  s3:=puppy.BigBark()
+	  s4:=puppy.BigBarks()
+      fmt.Println(s3)
+	  fmt.Println(s4)
 
+	  // also like this
 	  fmt.Println(puppy.Bark())
 	  fmt.Println(puppy.Barks())
 }
---
+++ LEARN-GO-V-THREE/go.mod
@@ -0,0 +1,7 @@
+module mymodule
+
+go 1.22.5
+
+require github.com/anuvara2000/puppy v0.0.0-20240905080924-1e6cbd9ab34a
+
+require github.com/anuvara2000/dog v0.0.0-20240904072408-3b23bee97b3b // indirect
